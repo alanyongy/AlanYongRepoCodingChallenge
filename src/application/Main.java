5d7d6ca986d7fd9ef3576f85ecc331fa05e87fea
HaopingZheng <haoping.zheng@ucalgary.ca>
2022-07-18 15:19:24 -0600

    Deleted Alan's branch to create a comflict.

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,7 +14,6 @@ public class Main extends Application {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Ryan's label"));
 			root.getChildren().add(new Label("Ryan's branch"));
-			root.getChildren().add(new Label("Alan's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Alan's project");
